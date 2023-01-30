4be4cc790559accad4ceb6f7e6e9d07f78d0618d
Philip Dudley <435890+pdud@users.noreply.github.com>
2023-01-30 15:18:49 -0500

    chore: move to glimmer components
    from https://github.com/cibernox/ember-power-select-typeahead/pull/90

--- addon/components/power-select-typeahead/trigger.js
+++ addon/components/power-select-typeahead/trigger.js
@@ -1,39 +1,43 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { isBlank } from '@ember/utils';
+import { action, get } from '@ember/object';
 import { schedule } from '@ember/runloop';
-import layout from '../../templates/components/power-select-typeahead/trigger';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  layout,
-  tagName: '',
-  text: '',
+export default class Trigger extends Component {
+  @tracked
+  text = '';
+
+  constructor(owner, args) {
+    super(owner, args);
+
+    this.oldSelect = args.select;
+  }
 
   /**
    * Lifecycle Hook
    * power-select updates the state of the publicAPI (select) for every typeahead
    * so we capture this as `state` via oldSelect && newSelect
-   *
-   * @private
-   * @method didReceiveAttrs
    */
-  didReceiveAttrs() {
-    this._super(...arguments);
-    let oldSelect = this.get('oldSelect');
-    let newSelect = this.set('oldSelect', this.get('select'));
-    if (!oldSelect) {
-      return;
-    }
+  @action
+  updatePublicApi(_elem, [select, extra]) {
+    let oldSelect = this.oldSelect;
+    this.oldSelect = select;
+    let newSelect = select;
+
     /*
      * We need to update the input field with value of the selected option whenever we're closing
      * the select box.
      */
     if (oldSelect.isOpen && !newSelect.isOpen && newSelect.searchText) {
-      let input = document.querySelector(`#ember-power-select-typeahead-input-${newSelect.uniqueId}`);
-      let newText = this.getSelectedAsText();
-      if (input.value !== newText) {
+      let input = document.querySelector(
+        `#ember-power-select-typeahead-input-${newSelect.uniqueId}`
+      );
+      let newText = getSelectedAsText(newSelect, extra);
+      if (input !== null && input.value !== newText) {
         input.value = newText;
       }
-      this.set('text', newText);
+      this.text = newText;
     }
 
     if (newSelect.lastSearchedText !== oldSelect.lastSearchedText) {
@@ -45,73 +49,69 @@ export default Component.extend({
     }
 
     if (oldSelect.selected !== newSelect.selected) {
-      this.set('text', this.getSelectedAsText());
+      this.text = getSelectedAsText(newSelect, extra);
     }
-  },
-
-  actions: {
-    /**
-     * on mousedown prevent propagation of event
-     *
-     * @private
-     * @method stopPropagation
-     * @param {Object} event
-     */
-    stopPropagation(e) {
-      e.stopPropagation();
-    },
-
-    /**
-     * called from power-select internals
-     *
-     * @private
-     * @method handleKeydown
-     * @param {Object} event
-     */
-    handleKeydown(e) {
-      // up or down arrow and if not open, no-op and prevent parent handlers from being notified
-      if ([38, 40].indexOf(e.keyCode) > -1 && !this.get('select.isOpen')) {
-        e.stopPropagation();
-        return;
-      }
-      let isLetter = e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32; // Keys 0-9, a-z or SPACE
-      // if isLetter, escape or enter, prevent parent handlers from being notified
-      if (isLetter || [13, 27].indexOf(e.keyCode) > -1) {
-        let select = this.get('select');
-        // open if loading msg configured
-        if (!select.isOpen && this.get('loadingMessage')) {
-          schedule('actions', null, select.actions.open);
-        }
-        e.stopPropagation();
-      }
-
-      // optional, passed from power-select
-      let onkeydown = this.get('onKeydown');
-      if (onkeydown && onkeydown(e) === false) {
-        return false;
-      }
-    }
-  },
+  }
 
   /**
-   * obtains seleted value based on complex object or primitive value from power-select publicAPI
+   * on mousedown prevent propagation of event
    *
    * @private
-   * @method getSelectedAsText
+   * @method stopPropagation
+   * @param {Object} event
    */
-  getSelectedAsText() {
-    let labelPath = this.get('extra.labelPath');
-    let value;
-    if (labelPath) {
-      // complex object
-      value = this.get(`select.selected.${labelPath}`);
-    } else {
-      // primitive value
-      value = this.get('select.selected');
-    }
-    if (value === undefined) {
-      value = '';
-    }
-    return value;
+  @action
+  stopPropagation(e) {
+    e.stopPropagation();
   }
-});
+
+  /**
+   * called from power-select internals
+   *
+   * @private
+   * @method handleKeydown
+   * @param {Object} event
+   */
+  @action
+  handleKeydown(e) {
+    // up or down arrow and if not open, no-op and prevent parent handlers from being notified
+    if ([38, 40].indexOf(e.keyCode) > -1 && !this.args.select.isOpen) {
+      e.stopPropagation();
+      return;
+    }
+    let isLetter = (e.keyCode >= 48 && e.keyCode <= 90) || e.keyCode === 32; // Keys 0-9, a-z or SPACE
+    // if isLetter, escape or enter, prevent parent handlers from being notified
+    if (isLetter || [13, 27].indexOf(e.keyCode) > -1) {
+      // open if loading msg configured
+      if (!this.args.select.isOpen && this.args.loadingMessage) {
+        schedule('actions', null, this.args.select.actions.open);
+      }
+      e.stopPropagation();
+    }
+
+    // optional, passed from power-select
+    if (this.args.onKeydown && this.args.onKeydown(e) === false) {
+      return false;
+    }
+    return;
+  }
+}
+
+/**
+ * obtains selected value based on complex object or primitive value from power-select publicAPI
+ */
+function getSelectedAsText(select, extra) {
+  let labelPath = extra?.labelPath;
+  let value;
+  if (labelPath != null && select.selected) {
+    // complex object
+    value = get(select.selected, labelPath);
+  } else {
+    // primitive value
+    value = select.selected;
+  }
+  if (value === undefined) {
+    value = '';
+  }
+  return value;
+}
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import RSVP from 'rsvp';
 import { later } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-const numbers = [
+const NUMBERS = [
   'one',
   'two',
   'three',
@@ -22,10 +24,10 @@ const numbers = [
   'seventeen',
   'eighteen',
   'nineteen',
-  'twenty'
+  'twenty',
 ];
 
-const users = [
+const USERS = [
   { name: 'Arthur' },
   { name: 'Sam' },
   { name: 'Dan' },
@@ -35,50 +37,70 @@ const users = [
   { name: 'Kirill' },
   { name: 'Stuart' },
   { name: 'Jamie' },
-  { name: 'Matteo' }
+  { name: 'Matteo' },
 ];
-const extra = { labelPath: 'name' };
+const EXTRA = { labelPath: 'name' };
 
-export default Controller.extend({
-  numbers,
-  users,
-  extra,
+export default class ApplicationController extends Controller {
+  @tracked
+  user;
 
-  actions: {
-    skipShortSearches(term, select) {
-      if (term.length <= 2) {
-        select.actions.search('');
-        return false;
-      }
-    },
+  @tracked
+  user2;
 
-    search(term) {
-      return numbers.filter((num) => num.indexOf(term) > -1);
-    },
+  @tracked
+  user3;
 
-    searchAsync(term) {
-      return new RSVP.Promise(function(resolve) {
-        if (term.length === 0) {
-          resolve([]);
-        } else {
-          later(function() {
-            resolve(numbers.filter((num) => num.indexOf(term) > -1));
-          }, 600);
-        }
-      });
-    },
+  get numbers() {
+    return NUMBERS;
+  }
 
-    searchUsersAsync(term) {
-      // return users.filter(u => u.name.indexOf(term) > -1);
-      return new RSVP.Promise(function(resolve) {
-        if (term.length === 0) {
-          resolve([]);
-        } else {
-          later(function() {
-            resolve(users.filter((u) => u.name.indexOf(term) > -1));
-          }, 600);
-        }
-      });
+  get users() {
+    return USERS;
+  }
+
+  get extra() {
+    return EXTRA;
+  }
+
+  @action
+  skipShortSearches(term, select) {
+    if (term.length <= 2) {
+      select.actions.search('');
+      return false;
     }
   }
-});
+
+  @action
+  search(term) {
+    return NUMBERS.filter((num) => num.indexOf(term) > -1);
+  }
+
+  @action
+  searchAsync(term) {
+    return new RSVP.Promise(function (resolve) {
+      if (term.length === 0) {
+        resolve([]);
+      } else {
+        later(function () {
+          resolve(NUMBERS.filter((num) => num.indexOf(term) > -1));
+        }, 600);
+      }
+    });
+  }
+
+  @action
+  searchUsersAsync(term) {
+    console.log('search', term);
+    // return users.filter(u => u.name.indexOf(term) > -1);
+    return new RSVP.Promise(function (resolve) {
+      if (term.length === 0) {
+        resolve([]);
+      } else {
+        later(function () {
+          resolve(USERS.filter((u) => u.name.indexOf(term) > -1));
+        }, 600);
+      }
+    });
+  }
+}
